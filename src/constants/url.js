5c963e7e49379e931286b405e593386d60bf3e40
Subhranandan Deka <subhradeka8@gmail.com>
2026-01-28 13:39:49 +0530

    Initial Commit

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,7 @@
 export const URL = "http://192.168.5.38:5000/";
 export const DUMMY_URL = "http://192.168.5.37:3333/";
 export const HOSTNAME = "192.168.5.38";
-export const IMG_URL = "https://global.toyota/pages/news/older/images/1993/05/24/001_2.jpg";
+export const IMG_URL =
+  "https://global.toyota/pages/news/older/images/1993/05/24/001_2.jpg";
+export const PORT = 5173;
+
